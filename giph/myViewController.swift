23ce1e214aa22ece4e96c0e6108efa6deba785b5
LandingHeart <land.liu0123@gmail.com>
2019-03-29 13:37:28 -0400

    new file

---
+++ giph/myViewController.swift
@@ -0,0 +1,30 @@
+//
+//  myViewController.swift
+//  giph
+//
+//  Created by Shinan Liu on 3/29/19.
+//  Copyright © 2019 Shinan Liu. All rights reserved.
+//
+
+import UIKit
+
+class myViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
