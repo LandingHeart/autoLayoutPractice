dd9839b7ce0300b9c2ed05ff2b523cb6853a8f7b
LandingHeart <land.liu0123@gmail.com>
2019-03-29 13:35:59 -0400

    files

---
+++ giph/mainViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  liveGif
+//
+//  Created by Shinan Liu on 3/21/19.
+//  Copyright © 2019 Shinan Liu. All rights reserved.
+//
+import UIKit
+
+class mainViewController: UIViewController {
+    
+    private let loginContentView:UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    private let unameTxtField:UITextField = {
+        let txtField = UITextField()
+        txtField.backgroundColor = .white
+        txtField.borderStyle = .roundedRect
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        return txtField
+    }()
+    private let pwordTxtField: UITextField = {
+        let txtField = UITextField()
+        txtField.borderStyle = .roundedRect
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        return txtField
+    }()
+    let btnLogin: UIButton = {
+        let btn = UIButton(type:.system)
+        btn.backgroundColor = .blue
+        btn.setTitle("Login", for: .normal)
+        btn.tintColor = .white
+        btn.layer.cornerRadius = 5
+        btn.clipsToBounds = true
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(mainViewController.buttonAction(_:)), for: .touchUpInside)
+        
+        
+        return btn
+    }()
+    let btn1: UIButton = {
+        let btn = UIButton(type:.system)
+        btn.backgroundColor = .lightGray
+        btn.setTitle("press me", for: .normal)
+        btn.tintColor = .white
+        btn.layer.cornerRadius = 5
+        btn.clipsToBounds = true
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(mainViewController.buttonAction(_:)), for: .touchUpInside)
+        
+    
+        
+        return btn
+        
+    }()
+    @objc func buttonAction(_ sender:UIButton!)
+    {
+        print("Button tapped")
+        let vc = ViewController() //your view controller
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    func setUpAutoLayout() {
+        loginContentView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        loginContentView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        loginContentView.heightAnchor.constraint(equalToConstant: view.frame.height/3 - 40).isActive = true
+        loginContentView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        unameTxtField.topAnchor.constraint(equalTo:loginContentView.topAnchor, constant:40).isActive = true
+        
+        unameTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        unameTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        unameTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
+        
+        pwordTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        pwordTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        pwordTxtField.topAnchor.constraint(equalTo:unameTxtField.bottomAnchor, constant:20).isActive = true
+        pwordTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
+        
+        btnLogin.topAnchor.constraint(equalTo:pwordTxtField.bottomAnchor, constant:20).isActive = true
+        btnLogin.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        btnLogin.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        btnLogin.heightAnchor.constraint(equalToConstant:50).isActive = true
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .gray
+        loginContentView.addSubview(unameTxtField)
+        loginContentView.addSubview(pwordTxtField)
+        loginContentView.addSubview(btnLogin)
+        
+        view.addSubview(loginContentView)
+        view.addSubview(loginContentView)
+        
+        setUpAutoLayout()
+    }
+    
+    
+}
