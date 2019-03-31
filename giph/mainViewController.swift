f8891493fdcfbaf2b931c259b5eb8f9a6796ae51
LandingHeart <land.liu0123@gmail.com>
2019-03-31 17:32:37 -0400

    constraints fix

--- giph/mainViewController.swift
+++ giph/mainViewController.swift
@@ -15,19 +15,19 @@ class mainViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    private let unameTxtField:UITextField = {
-        let txtField = UITextField()
-        txtField.backgroundColor = .white
-        txtField.borderStyle = .roundedRect
-        txtField.translatesAutoresizingMaskIntoConstraints = false
-        return txtField
-    }()
-    private let pwordTxtField: UITextField = {
-        let txtField = UITextField()
-        txtField.borderStyle = .roundedRect
-        txtField.translatesAutoresizingMaskIntoConstraints = false
-        return txtField
-    }()
+//    private let unameTxtField:UITextField = {
+//        let txtField = UITextField()
+//        txtField.backgroundColor = .white
+//        txtField.borderStyle = .roundedRect
+//        txtField.translatesAutoresizingMaskIntoConstraints = false
+//        return txtField
+//    }()
+//    private let pwordTxtField: UITextField = {
+//        let txtField = UITextField()
+//        txtField.borderStyle = .roundedRect
+//        txtField.translatesAutoresizingMaskIntoConstraints = false
+//        return txtField
+//    }()
     
     let btn1: UIButton = {
         let btn = UIButton(type:.system)
@@ -49,37 +49,37 @@ class mainViewController: UIViewController {
     }
     
     func setUpAutoLayout() {
-        loginContentView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
-        loginContentView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
-        loginContentView.heightAnchor.constraint(equalToConstant: view.frame.height/3 - 20).isActive = true
-        loginContentView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//        loginContentView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+//        loginContentView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+//        loginContentView.heightAnchor.constraint(equalToConstant: view.frame.height/3 - 20).isActive = true
+//        loginContentView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//
+//        unameTxtField.topAnchor.constraint(equalTo:loginContentView.topAnchor, constant:40).isActive = true
+//
+//        unameTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+//        unameTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+//        unameTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
+//
+//        pwordTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+//        pwordTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+//        pwordTxtField.topAnchor.constraint(equalTo:unameTxtField.bottomAnchor, constant:20).isActive = true
+//        pwordTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
+//
+//
+//
+        let margins = view.layoutMarginsGuide
+        btn1.translatesAutoresizingMaskIntoConstraints = false;
         
-        unameTxtField.topAnchor.constraint(equalTo:loginContentView.topAnchor, constant:40).isActive = true
-        
-        unameTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
-        unameTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
-        unameTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
-        
-        pwordTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
-        pwordTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
-        pwordTxtField.topAnchor.constraint(equalTo:unameTxtField.bottomAnchor, constant:20).isActive = true
-        pwordTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
-        
-       
-        
-        btn1.topAnchor.constraint(equalTo:pwordTxtField.bottomAnchor, constant:20).isActive = true
-        btn1.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
-        btn1.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
-        btn1.heightAnchor.constraint(equalToConstant:50).isActive = true
+        btn1.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -50).isActive = true
+        btn1.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        btn1.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = .gray
-        loginContentView.addSubview(unameTxtField)
-        loginContentView.addSubview(pwordTxtField)
+        view.backgroundColor = .white
         
-        view.addSubview(loginContentView)
+        
      
         view.addSubview(btn1)
         setUpAutoLayout()
